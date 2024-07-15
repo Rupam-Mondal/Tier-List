64af15ffcba9e40d9c6a9138180b22163b652181
Rupam <rupammondal564@gmail.com>
2024-07-15 10:41:36 +0530

    Image addition done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//All work of tier list addition
 const usergiventiername = document.getElementById("userinput-tier")
 
 const tieraddbutton = document.getElementById("tier-add-button")
@@ -34,4 +35,42 @@ function tiercreation(){
     tiername.appendChild(name)
     name.textContent = usergiventiername.value
     usergiventiername.value = ""
+}
+
+//All work of image addition
+const imagegivenuser = document.querySelector(".img-addition input")
+const imageaddbutton = document.querySelector(".new-img")
+
+imageaddbutton.addEventListener('click' , () => {
+    if(imagegivenuser.value == ""){
+        alert("Enter valid image url")
+    }
+    addimage(imagegivenuser.value)
+})
+
+function addimage(url){
+    imagegivenuser.value = ""
+    const imagediv = document.createElement('div')
+    const newimage = document.createElement("img")
+    newimage.setAttribute('draggable' , 'true')
+    imagediv.classList.add('realimage')
+    newimage.src = url
+    imagediv.appendChild(newimage)
+    const parentimgsection = document.querySelector(".img-section-main")
+    parentimgsection.appendChild(imagediv)
+}
+
+//All work of drag event
+
+const allimages = document.getElementsByClassName("realimage")
+console.log(allimages)
+
+for(const iterator of allimages){
+    setupfordrag(iterator)
+}
+
+function setupfordrag(iterator){
+    iterator.addEventListener('dragstart' , () => {
+        
+    })
 }
